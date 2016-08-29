c47fca8f4425875585f3527ad705ed3e4a8239b9
CHOIJAE HOON <hoonja@gmail.com>
2016-08-29 20:22:27 +0900

    1. MapViewController 추가 2. viewDidLoad() overriding

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -61,4 +61,10 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
             return true
         }
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ConversionViewController loaded its view.")
+    }
 }
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by CHOIJAE HOON on 2016. 8. 29..
+//  Copyright © 2016년 CHOIJAE HOON. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
